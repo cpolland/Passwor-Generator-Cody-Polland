50a7b090313a18463cd6670e6cd01163c07a9fb6
Cody Polland <cody.polland@gmail.com>
2022-07-19 12:52:10 -0700

    Updated Redo of Project

--- script.js
+++ script.js
@@ -1,107 +1,93 @@
-var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V','W','X','Y','Z',];
-var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var special = ["!","@","#","$","%","^","&","*","(",")"];
+var upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 
-var generateBtn = document.querySelector('#generate');
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-generateBtn.addEventListener('click', writePassword);
-
-
-function randomizer(arr) {
-  var randIndex = Math.floor(Math.random() * arr.length);
-  var randElement = arr[randIndex];
-
-  return randElement;
-}
-
-
-function createPassword() {
-  var choices = passWordCriteria();
-  var outcomes = [];
-  var potentialCharacters = [];
-  var definateCharacters = [];
-  if (!choices) return null;
-
-  if (choices.needSpecialCharacters) {
-    potentialCharacters = potentialCharacters.concat(special);
-    definateCharacters.push(randomizer(special));
-  }if (choices.hasNumbers) {
-    potentialCharacters = potentialCharacters.concat(numbers);
-    definateCharacters.push(randomizer(numbers));
-  }
-  if (choices.needLowercaseCharacters) {
-    potentialCharacters = potentialCharacters.concat(lowerCase);
-    definateCharacters.push(randomizer(lowerCase));
-  } if (choices.needUppercaseCharacters) {
-    potentialCharacters = potentialCharacters.concat(upperCase);
-    definateCharacters.push(randomizer(upperCase));
-  }
-
-  for (var i = 0; i < choices.length; i++) {
-    var possibleCharacter = randomizer(potentialCharacters);
-
-    outcomes.push(possibleCharacter);
-  }
-
-  for (var i = 0; i < definateCharacters.length; i++) {
-    outcomes[i] = definateCharacters[i];
-  }
-  return outcomes.join('');
-}
-
-function passWordCriteria() {
-  var passWordCriteriaLength = parseInt(
-    prompt('How long would you like your password to be?'),
+function passwordPrompt() {
+  var passwordLength = parseInt(
+    prompt("How long would you like you password to be?"),
+    10
   );
-  if (Number.isNaN(passWordCriteriaLength)) {
-    alert('Please choose a number between 1 and 8');
+  if (Number.isNaN(passwordLength)) {
+    alert("Password must be a number.");
     return null;
   }
-  if (passWordCriteriaLength < 8) {
-    alert('Password length must be at least 8 characters');
+  if (passwordLength < 10) {
+    alert("Password must be at least 10 characters long.");
     return null;
   }
-  var needSpecialCharacters = confirm(
-    'Add special Characters?'
-  );
-  var hasNumbers = confirm(
-    'Add Numbers.'
-  );
-  var needLowercaseCharacters = confirm(
-    'Add lowercase Characters.'
-  );
-  var needUppercaseCharacters = confirm(
-    'Add uppercase characters.'
-  );
-  if (
-    needSpecialCharacters === false &&
-    hasNumbers === false &&
-    needLowercaseCharacters === false &&
-    needUppercaseCharacters === false
-  ) {
-    alert('please pick on type');
-    return null;
-  }
+}
+function generatePassword() {
+  var password = "password";
+  // TODO: add code to generate the password here
 
- 
-  var passwordObject = {
-    length: length,
-    needSpecialCharacters: needSpecialCharacters,
-    hasNumbers: hasNumbers,
-    needLowercaseCharacters: needLowercaseCharacters,
-    needUppercaseCharacters: needUppercaseCharacters,
-  };
-
-  return passwordObject;
+  return password;
 }
 
+// Write password to the #password input
 function writePassword() {
-  var password = createPassword();
-  var passwordText = document.querySelector('#password');
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
-
-
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
